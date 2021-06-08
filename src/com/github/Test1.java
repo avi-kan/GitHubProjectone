d92ac481422207e3f94339ce87035d0088b4d48a
Srikanth Nallamadugu <Srikanth Nallamadugu@LAPTOP-I681UD9V>
2021-06-08 14:27:54 +0530

    First Commit

---
+++ src/com/github/Test1.java
@@ -0,0 +1,17 @@
+package com.github;
+
+public class Test1 {
+	
+	public void test() {
+		String str="House";
+		
+		if(str == "House") {
+			System.out.println("Building");
+		}
+		else
+		{
+			System.out.println("palace");
+		}
+	}
+
+}
